ab5ee75ffd82817fa30afa3c62243b4a880b1402
Kanishk <kanishk1128.becse24@chitkara.edu.in>
2026-05-21 00:26:54 +0530

    Fixed upload path

--- backend-project/middleware/upload.js
+++ backend-project/middleware/upload.js
@@ -1,8 +1,21 @@
 const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const uploadPath = path.join(__dirname, "../public/uploads");
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "public/uploads"),
-  filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname)
+  destination: function (req, file, cb) {
+    cb(null, uploadPath);
+  },
+
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
 });
 
 const upload = multer({ storage });
