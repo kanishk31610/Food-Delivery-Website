5a7330a7b4ed4ee793cee09a3bc4bb6e23b0d2fa
Kanishk <kanishk1128.becse24@chitkara.edu.in>
2026-05-21 01:35:44 +0530

    fix: update passport and env

--- backend-project/config/passport.js
+++ backend-project/config/passport.js
@@ -1,33 +1,42 @@
+require("dotenv").config();
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../models/User");
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback"
-},
-async (accessToken, refreshToken, profile, done) => {
+const callbackURL =
+  process.env.NODE_ENV === "production"
+    ? "https://food-delivery-website-sepia-eight.vercel.app/auth/google/callback"
+    : "http://localhost:3000/auth/google/callback";
 
-  const email = profile.emails[0].value;
-  const googlePic = profile.photos?.[0]?.value?.replace("s96-c", "s400-c");
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: callbackURL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const email = profile.emails[0].value;
+      const googlePic = profile.photos?.[0]?.value?.replace("s96-c", "s400-c");
 
-  let user = await User.findOne({ email });
+      let user = await User.findOne({ email });
 
-  if (!user) {
-    user = await User.create({
-      username: profile.displayName,
-      email,
-      password: "google",
-      profilePic: googlePic || "/images/default.png"
-    });
-  } else {
-    user.profilePic = googlePic || user.profilePic;
-    await user.save();
-  }
+      if (!user) {
+        user = await User.create({
+          username: profile.displayName,
+          email,
+          password: "google",
+          profilePic: googlePic || "/images/default.png",
+        });
+      } else {
+        user.profilePic = googlePic || user.profilePic;
+        await user.save();
+      }
 
-  return done(null, user);
-}));
+      return done(null, user);
+    },
+  ),
+);
 
 passport.serializeUser((user, done) => done(null, user.id));
 passport.deserializeUser(async (id, done) => {
